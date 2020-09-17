94a13a79cfa794ba36163884aff485613da7049c
mohsin khan <khan.mohsin.00001@gmail.com>
2020-09-17 12:09:25 +0530

    added more files

--- controllers/users_controllers.js
+++ controllers/users_controllers.js
@@ -55,7 +55,31 @@ User.findOne({email:req.body.email}, function(err,user){
     }
 
 
-// sign and create a session for user
+// sign in and create a session for user
 module.exports.createSession=function(req,res){
+// steps to authenticate
+// find the user
+User.findOne({email:req.body.email},function(err,user) {
 
+ if(err){console.log('error in finding in user signing in'); return}
+    
+ // handle user found
+
+ if(user){
+     // handle passord which does not match
+  if(user.password!=req.body.password)
+  {
+      return res.redirect('back');
+  }
+
+     // handle session creation
+     res.cookie('user_id',user._id);
+     console.log('succesfully logged in  *****');
+     return res.redirect('/user/profile');
+ }
+ else{
+     // handle user is not found
+     return res.redirect('back');
+ }
+});
 }
