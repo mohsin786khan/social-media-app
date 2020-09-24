a91c8823d06558f6f363ab207ec91448699296da
mohsin khan <khan.mohsin.00001@gmail.com>
2020-09-25 02:19:25 +0530

    create a post

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,7 +1,24 @@
-module.exports.home=function(req,res){
-   console.log('R',req.cookies);
-   return res.render('home',{
-    title:"major"
-   });
+const Post=require('../models/post');
 
+
+module.exports.home=function(req,res){
+//   console.log('R',req.cookies);
+
+//    Post.find({},function(err,posts){
+//       return res.render('home',{
+//          title:"major",
+//           posts: posts
+//    });
+//  });
+ 
+
+// popualate the user of each post
+Post.find({}).populate('user').exec(function(err,posts) {
+   return res.render('home',{
+                  title:"major",
+                  posts: posts
+          });
+}
+
+);
 }
--- controllers/users_controllers.js
+++ controllers/users_controllers.js
@@ -56,9 +56,9 @@ module.exports.create = function(req, res){
 
 
 // sign in and create a session for the user
-module.exports.createSession = function(req, res){
-    return res.redirect('/');
-}
+  module.exports.createSession = function(req, res){
+     return res.redirect('/');
+  } 
 
 module.exports.destroySession = function(req, res){
     req.logout();
