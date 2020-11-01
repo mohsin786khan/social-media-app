295a269fd93cc06080518eddc11123b2cd8d4df4
mohsin khan <khan.mohsin.00001@gmail.com>
2020-11-01 12:41:19 +0530

    add flash message

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -3,20 +3,20 @@ const User = require('../models/user');
 
 
 
-    
 module.exports.home = async function(req, res){
-    try{
-    // populate the user of each post
-  let posts=await  Post.find({})
-    .populate('user')
-    .populate({
-        path: 'comments',
-        populate: {
-            path: 'user'
-        }
-    });
 
-    let users=  await User.find({});
+    try{
+         // populate the user of each post
+        let posts = await Post.find({})
+        .populate('user')
+        .populate({
+            path: 'comments',
+            populate: {
+                path: 'user'
+            }
+        });
+    
+        let users = await User.find({});
 
         return res.render('home', {
             title: "Codeial | Home",
@@ -24,13 +24,19 @@ module.exports.home = async function(req, res){
             all_users: users
         });
 
-}
-catch(err)
-{
-console.log('error',err);
-return;
-}
-
+    }catch(err){
+        console.log('Error', err);
+        return;
+    }
+   
 }
 
 // module.exports.actionName = function(req, res){}
+
+
+// using then
+// Post.find({}).populate('comments').then(function());
+
+// let posts = Post.find({}).populate('comments').exec();
+
+// posts.then()
--- routes/comments.js
+++ routes/comments.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const passport = require('passport');
 
-const commentsController = require('../controllers/comment_controller');
+const commentsController = require('../controllers/comments_controller');
 
 router.post('/create', passport.checkAuthentication, commentsController.create);
 router.get('/destroy/:id',passport.checkAuthentication,commentsController.destroy);
