377128bf93eee3d499db5674f6c20118c60f17eb
mohsin khan <khan.mohsin.00001@gmail.com>
2020-11-11 21:10:15 +0530

    add sign in with google account

--- routes/api/v1/posts.js
+++ routes/api/v1/posts.js
@@ -6,3 +6,4 @@ const postApi=require("../../../controllers/api/v1/post_api");
 router.get('/', postApi.index);
 router.delete('/:id',passport.authenticate('jwt',{session:false}), postApi.destroy);
 module.exports=router;
+
--- routes/users.js
+++ routes/users.js
@@ -24,4 +24,8 @@ router.get('/sign-out', usersController.destroySession);
 
 module.exports = router;
 
-/// vo link pehle se work nhi karraha h use me handle karlunga baad me ok ek sec ruko ok
+
+
+router.get('/auth/google', passport.authenticate('google', {scope: ['profile', 'email']}));
+router.get('/auth/google/callback', passport.authenticate('google', {failureRedirect: '/users/sign-in'}), usersController.createSession);
+
